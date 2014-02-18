d8af18ce8a52c720836ab108b4f8ec656f405e9c
Zhan Yap <zhanhui913@gmail.com>
2014-02-17 18:15:17 -0700

    Implementing facebook logout

--- example.php
+++ example.php
@@ -51,7 +51,7 @@ if ($user) {
 }
 
 // This call will always work since we are fetching public data.
-//$zhan = $facebook->api('/zhanny913');
+$zhan = $facebook->api('/shenyapp');
 
 ?>
 <!doctype html>
